12634a4f15e1bcdc0f49dd8a10867528965306d3
Didsen1 <tihanovart@gmail.com>
2023-09-18 01:34:10 +0300

    Еще одна попытка 14 пр

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 
-const routeUsers = require('./routes/users');
-const routeCards = require('./routes/cards');
 const routeSignin = require('./routes/signin');
 const routeSignup = require('./routes/signup');
+
 const auth = require('./middlewares/auth');
+
+const routeUsers = require('./routes/users');
+const routeCards = require('./routes/cards');
 
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
@@ -26,8 +28,8 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', routeSignup);
-app.post('/', routeSignin);
+app.use('/', routeSignup);
+app.use('/', routeSignin);
 
 app.use(auth);
 
