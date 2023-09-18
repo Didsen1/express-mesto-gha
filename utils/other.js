38ae01c7ec225d1f290afe2ad682ce4bf71ad775
Didsen1 <tihanovart@gmail.com>
2023-09-18 23:14:14 +0300

    Попытка исправить удаление 13

--- utils/other.js
+++ utils/other.js
@@ -1,4 +1,4 @@
-const SECRET_SIGNING_KEY = 'pBXCxQ4a2HJztwAuOF0LRcmEboTnq3jf';
+const SECRET_SIGNING_KEY = 'U2FsdGVkX19nV2KreWqHk1BGD+ojOGgl39N93rtj1DVVSeYcdNPGnAQt4PtU2FvJrEiPtoQRACJ0B/yIORhjvQ==';
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 module.exports = {
